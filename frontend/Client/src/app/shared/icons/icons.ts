fca2598c8964e1a9b2f015018ec7e025cb5911d6
Mohammed-Hassan <158804620+Mohammed-Bata@users.noreply.github.com>
2026-02-19 18:31:03 +0200

    Delete frontend/Client directory

--- frontend/Client/src/app/shared/icons/icons.ts
+++
@@ -1,9 +0,0 @@
-export const icons = [
-    '../../../../assets/icons/ProjectsIcons/Alian.svg',
-    '../../../../assets/icons/ProjectsIcons/Bird.svg',
-    '../../../../assets/icons/ProjectsIcons/Cloud.svg',
-    '../../../../assets/icons/ProjectsIcons/Code.svg',
-    '../../../../assets/icons/ProjectsIcons/Coffee.svg',
-    '../../../../assets/icons/ProjectsIcons/Design.svg',
-    '../../../../assets/icons/ProjectsIcons/Disc.svg'
-]
